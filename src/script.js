74306ad757326305535c61cab8033f36a3cfe62e
RETR0 DEDSEC <eduardoviktor0@outlook.com>
2022-11-26 14:54:44 -0300

    UI0.2

--- src/script.js
+++ src/script.js
@@ -1,21 +1,97 @@
-const side_bar = document.getElementById("side-bar");
-const content_view = document.getElementById("content-view");
+window.onload = () => {
+  const form1 = document.querySelector("#addForm");
 
-let innerHeight = `${window.innerHeight}px`;
-let innerWidth = `${window.innerWidth}px`;
-content_view.style.width = `${window.innerWidth}px`;
-side_bar.style.height = `${window.innerHeight}px`;
+  let items = document.getElementById("items");
+  let submit = document.getElementById("submit");
 
-function add_todo() {
-  console.log("Test");
-}
-function resize() {
-  if (`${window.innerWidth}px` !== innerWidth) {
-    content_view.style.width = `${window.innerWidth}px`;
+  let editItem = null;
+
+  form1.addEventListener("submit", addItem);
+  items.addEventListener("click", removeItem);
+};
+
+function addItem(e) {
+  e.preventDefault();
+
+  if (submit.value != "Submit") {
+    console.log("Hello");
+
+    editItem.target.parentNode.childNodes[0].data
+      = document.getElementById("item").value;
+
+    submit.value = "Submit";
+    document.getElementById("item").value = "";
+
+    document.getElementById("lblsuccess").innerHTML
+      = "Text edited successfully";
+
+    document.getElementById("lblsuccess")
+      .style.display = "block";
+
+    setTimeout(function () {
+      document.getElementById("lblsuccess")
+        .style.display = "none";
+    }, 3000);
+
+    return false;
   }
-  else if (`${window.innerHeight}px` !== innerHeight)
-  {
-    side_bar.style.height = `${window.innerHeight}px`;
+
+  let newItem = document.getElementById("item").value;
+  if (newItem.trim() == "" || newItem.trim() == null)
+    return false;
+  else
+    document.getElementById("item").value = "";
+
+  let li = document.createElement("li");
+  li.className = "list-group-item";
+
+  let deleteButton = document.createElement("button");
+
+  deleteButton.className =
+    "btn-danger btn btn-sm float-right delete";
+
+  deleteButton.appendChild(document.createTextNode("Delete"));
+
+  let editButton = document.createElement("button");
+
+  editButton.className =
+    "btn-success btn btn-sm float-right edit";
+
+  editButton.appendChild(document.createTextNode("Edit"));
+
+  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(deleteButton);
+  li.appendChild(editButton);
+
+  items.appendChild(li);
+}
+
+function removeItem(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you Sure?")) {
+      let li = e.target.parentNode;
+      items.removeChild(li);
+      document.getElementById("lblsuccess").innerHTML
+        = "Text deleted successfully";
+
+      document.getElementById("lblsuccess")
+        .style.display = "block";
+
+      setTimeout(function () {
+        document.getElementById("lblsuccess")
+          .style.display = "none";
+      }, 3000);
+    }
+  }
+  if (e.target.classList.contains("edit")) {
+    document.getElementById("item").value =
+      e.target.parentNode.childNodes[0].data;
+    submit.value = "EDIT";
+    editItem = e;
   }
 }
-setTimeout(resize, 1)
+
+function toggleButton(ref, btnID) {
+  document.getElementById(btnID).disabled = false;
+}
